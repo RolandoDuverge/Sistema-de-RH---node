9120cb19be9c9286df0fc41c780acda7d6857674
Rolando Duverge <arturo1872002@gmail.com>
2022-09-12 14:45:30 -0400

    fix register

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const conexion = require("./database/db");
+const bcryptjs = require('bcryptjs');
 
 router.get('/', (req, res) => {
   conexion.query('SELECT * FROM idiomas',(error, results)=>{
@@ -49,9 +50,34 @@ router.get('/register', (req, res)=>{
 })
 
 const crud = require('./controllers/crud');
+const { application } = require("express");
 router.post('/save', crud.save)
 router.post('/update', crud.update)
 
+router.post('/register', async (req, res)=>{
+  const user = req.body.user;
+  const name = req.body.name;
+  const rol = req.body.rol;
+  const pass = req.body.pass;
+
+  let passwordHaash = await bcryptjs.hash(pass, 8);
+  conexion.query('INSERT INTO users SET ?', {user:user, name:name, rol:rol, pass:passwordHaash}, async(error, results)=>{
+    if (error) {
+      throw error;
+    } else {
+      res.render('register', {
+				alert: true,
+				alertTitle: "Registration",
+				alertMessage: "¡Successful Registration!",
+				alertIcon:'success',
+				showConfirmButton: false,
+				timer: 1500,
+				ruta: ''
+			});
+    }
+  })
+
+})
 
 module.exports = router;
 
